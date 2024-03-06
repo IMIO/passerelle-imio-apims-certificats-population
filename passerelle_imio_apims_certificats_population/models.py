0d5b6645d273acaa9c71531d6216ca196b7a4482
Jordano Modesto <jordano.modesto@imio.be>
2024-03-06 14:12:43 +0100

    [TELE-1888]Connecter created

--- passerelle_imio_apims_certificats_population/models.py
+++ passerelle_imio_apims_certificats_population/models.py
@@ -29,7 +29,7 @@ class ApimsCertificatsPopulationConnector(BaseResource):
         username used to connect to APIMS
     password : str
         password used to connect to APIMS
-    municipality_nis_code : str
+    municipality_token : str
         token used to identify municipality to APIMS
     Methods
     -------
@@ -54,11 +54,11 @@ class ApimsCertificatsPopulationConnector(BaseResource):
         help_text="Mot de passe APIMS Certificats Population",
         verbose_name="Mot de passe",
     )
-    municipality_nis_code = models.CharField(
+    municipality_token = models.CharField(
         max_length=128,
         blank=True,
-        help_text="Code NIS d'identification de l'organisme dans APIMS Certificats Population",
-        verbose_name="Code NIS de l'organisme",
+        help_text="Slug d'identification de l'organisme dans APIMS Certificats Population",
+        verbose_name="slug de l'instance",
     )
 
     category = 'Connecteurs iMio'
@@ -67,118 +67,73 @@ class ApimsCertificatsPopulationConnector(BaseResource):
 
     class Meta:
         verbose_name = 'Connecteur APIMS Certificats Population'
-    # Define a method that gives an access to http session
-    @property
-    def session(self):
-        session = requests.Session()
-        session.auth = (self.username, self.password)
-        session.headers.update({
-            "Accept": "application/json",
-            "X-IMIO-MUNICIPALITY-NIS": self.municipality_nis_code
-        })
-        return session
 
     # endpoint connector
     @endpoint(
-        name="document-types",
+        name="read-document",
         perm="can_access",
         methods=["get"],
         description="Obtenir un certificat de population",
         parameters={
             "document_type": {
-                "description": "Type de cerficat de population",
+                "description": "Type de cerficats de population",
                 "example_value": "LegalCohabition",
             },
             "person_nrn": {
                 "description": "Numéro de registre national de la personne qui est concernée par la demande de document de type certificat de population",
-                "example_value": "15010123487",
+                "example_value": "76070935550",
             },
             "requestor_nrn": {
                 "description": "Numéro de registre national de la personne qui demande un document de type certificat de population",
-                "example_value": "15010123487",
-            },
-            "commune_nis": {
-                "description": "Code NIS de la commune",
-                "example_value": "51063",
+                "example_value": "76070935550",
             },
         },
         display_order=1,
         display_category="Documents"
     )
-    def get_document(self, request, document_type, person_nrn, requestor_nrn, commune_nis=None, language="fr"):
+    def read_document(self, request, document_type, person_nrn, requestor_nrn):
         """ Get asked json document
         Parameters
         ----------
         document_type : str
-            Extract's code
+            certificat type
         person_nrn : str
             National number for the extract person
         requestor_nrn : str
             National number of the requester
-        language : str
-            Language of the document
         Returns
         -------
         JSON
         """
-        if commune_nis is None:
-            commune_nis = self.municipality_nis_code
+        municipality_token = self.municipality_token
 
         url = f"{self.url}/mon-dossier-documents/{person_nrn}/{document_type}"
 
-        self.logger.info("Récupération du JSON")
+        self.logger.info("Récupération du PDF")
         try:
             response = requests.get(
                 url,
                 auth=(self.username, self.password),
                 headers={
                     "X-IMIO-REQUESTOR-NRN": requestor_nrn,
-                    "X-IMIO-MUNICIPALITY-NIS": commune_nis
+                    "X-IMIO-MUNICIPALITY-TOKEN": municipality_token
                 },
-                params={"language": language}
             )
         except Exception as e:
-            self.logger.warning(f'certificats population APIMS Error: {e}')
-            raise APIError(f'certificats population APIMS Error: {e}')
+            self.logger.warning(f'NRN APIMS Error: {e}')
+            raise APIError(f'NRN APIMS Error: {e}')
 
-        json_response = None
-        try:
-            json_response = response.json()
-        except ValueError:
-            self.logger.warning('certificats population APIMS Error: bad JSON response')
-            raise APIError('certificats population APIMS Error: bad JSON response')
-
-        if response.status_code >= 500:
-            self.logger.warning(f'certificats population APIMS Error: {e} {json_response}')
-            raise APIError(f'certificats population APIMS Error: {e} {json_response}')
-
-        return json_response
-
-    @endpoint(
-        name="decode-extract",
-        perm="can_access",
-        methods=["post"],
-        description="Décoder le certificats population d'une personne",
-        display_order=1,
-        display_category="Documents"
-    )
-    def decode_extract(self, request):
-        """ Post decode document as PDF
-        Returns
-        -------
-        PDF document
-        """
-
-        self.logger.info("certificats population decode pdf base64")
-        body = json.loads(request.body)
-        pdf_base64 = body["pdf_base64"]
 
         pdf_response = None
         try:
-            pdf = base64.b64decode(pdf_base64)
-            pdf_response = HttpResponse(pdf, content_type="application/pdf")
+            pdf_response = HttpResponse(response.content, content_type="application/pdf")
         except ValueError:
-            self.logger.warning('certificats population APIMS Error: bad PDF response')
-            raise APIError('certificats population APIMS Error: bad PDF response')
+            self.logger.warning('NRN APIMS Error: bad PDF response')
+            raise APIError('NRN APIMS Error: bad PDF response')
 
+        try:
+            response.raise_for_status()
+        except Exception as e:
+            self.logger.warning(f'NRN APIMS Error: {e}')
+            raise APIError(f'NRN APIMS Error: {e}')
         return pdf_response
